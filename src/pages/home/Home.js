888a8f5bd5722865c1bf15fbb09e46cc8d5d8c21
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-08 21:17:39 +0200

    add

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import Header from '../../components/header/Header'
+import React, { useState, useEffect } from "react";
+import Header from "../../components/header/Header";
+import axios from "axios";
+
+const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+const APP_ID = "fc502a09";
+const APP_KEY = "2fc5a26a70816b42235fe8383e235f9e";
 
 const Home = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
-}
+  const [query, setQuery] = useState("apple");
+  const [recipes, setRecipes] = useState();
+  const [meal, setMeal] = useState(mealTypes[0].toLocaleLowerCase());
 
-export default Home
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await axios.get(url);
+      // console.log(result)
+      if (result.status === 200) {
+        // console.log(result.data.hits)
+        setRecipes(result.data.hits);
+      }
+    } else {
+      alert("Please fill the form");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <Header
+        setQuery={setQuery}
+        query={query}
+        getData={getData}
+        mealTypes={mealTypes}
+        setMeal={setMeal}
+        meal={meal}
+      />
+    </div>
+  );
+};
+
+export default Home;
