e3590c551fce8eaf0d38b79edb04351fbf32a0d9
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-08 21:42:12 +0200

    add

--- src/components/header/Form.js
+++ src/components/header/Form.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Button, FoodInput, FormContainer, Select } from "./HeaderStyle";
 
-const Form = () => {
+const Form = ({ setQuery, query, getData, mealTypes, setMeal, meal }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(handleSubmit)
+    getData();
   };
+
+  const handleChange = (e) => {
+    setMeal(e.target.value)
+  }
   return (
     <FormContainer onSubmit={handleSubmit}>
-      <FoodInput type="text" placeholder="Search" />
+      <FoodInput
+        type="text"
+        placeholder="Search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <Button>Search</Button>
-      <Select name="mealTypes" id="mealTypes"></Select>
+      <Select name="mealTypes" id="mealTypes" onChange={handleChange}>
+      {mealTypes?.map((item, index) => (
+        <option value={item.toLowerCase()} key={index}>{item}</option>
+      ))}
+      </Select>
     </FormContainer>
   );
 };
