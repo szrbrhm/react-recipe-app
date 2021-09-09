94b4f1d01c85a11115035f25cf8da748af46587c
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-09 19:50:06 +0200

    change comp

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,22 +1,37 @@
+import React, { useState } from "react";  
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import NavBar from "../components/navbar/NavBar";
-import Home from "../pages/home/Home";
+import Navbar from "../components/navbar/Navbar";
 import About from "../pages/about/About";
-import Details from "../Details/Details";
-import Login from "../pages/login/Login";
-
+import Home from "../pages/home/Home";
+import Details from "../pages/details/Details";
+import Login from "../pages/login/login";
+import PrivateRouter from './PrivateRouter';   
 function AppRouter() {
+  const [isAuth, setIsAuth] = useState(false)
+  const AuthContainer = () => (   
+    <div>
+      <Navbar />
+      <PrivateRouter isAuth={isAuth} exact path="/" component={Home} />
+      <PrivateRouter isAuth={isAuth} path="/about" component={About} />
+      <PrivateRouter isAuth={isAuth} path="/details" component={Details} />
+    </div>
+  );
   return (
     <BrowserRouter>
-      <NavBar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/details" exact component={Details} />
-        <Route path="/login" exact component={Login} />
+      <Route         
+          path="/login"
+          exact
+          component={() => <Login setIsAuth={setIsAuth} />}
+        />
+         <Route component={AuthContainer} />   
       </Switch>
     </BrowserRouter>
   );
 }
-
 export default AppRouter;
+
+{/* <Route path="/" exact component={Home} />
+<Route path="/about" exact component={About} />
+<Route path="/details" exact component={Details} />
+<Route path="/login" exact component={Login} /> */}
